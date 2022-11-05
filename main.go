89d02ce6912a71e192449b46171d0daa55241e83
Ian Evans <icevans@icloud.com>
2022-11-05 08:49:45 -0700

    feat: get basic deployments working

--- main.go
+++ main.go
@@ -93,7 +93,7 @@ func main() {
 		healthz.Module(services.Gin, services.Config, services.Log)
 		traefik.Module(services.Gin, configService, kubeService, log)
 		registries.Module(services.Gin, configService, kubeService, log)
-		deployments.Module(services.Gin, services.Streams, kubeService, log)
+		deployments.Module(services.Gin, kubeService, log)
 	}
 
 	// CleanUp
